80b9e777563a99f572aa02dcd73be6a21b7cdcd2
Roman <141589347+Romanru5116@users.noreply.github.com>
2026-02-21 11:03:45 +0300

    Create maxtrack v6.sql

---
+++ maxtrack v6.sql
@@ -0,0 +1,10 @@
+#выбрать самый длинный сингл из альбома
+SELECT T.name, T.duration_ms/1000/60 AS duration_minutes
+FROM music.d_track T
+WHERE T.album_spotify_id IN (
+   SELECT A.spotify_id
+   FROM music.d_album A
+   WHERE A.total_tracks = 1
+)
+ORDER BY T.duration_ms DESC
+LIMIT 1;
